4edb813a78fffc4040c36ae792f6775d32f847f8
mcquiddy <cristian95mc@gmail.com>
2016-10-23 04:11:54 -0600

    update logic index

--- webPage/js/index.js
+++ webPage/js/index.js
@@ -1,6 +1,7 @@
 var indexApp = angular.module('index',[])
 .controller('indexCtrl', ['$scope', '$http', function ($scope, $http) {
     var modal = document.getElementById('myModal');
+    var loginmodal = document.getElementById('loginModal');
 
     // Get the button that opens the modal
     var btn = document.getElementById("myBtn");
@@ -25,37 +26,96 @@ var indexApp = angular.module('index',[])
         }
     }
     
-    var mensaje;
+    var mensaje = {};
+    var roles;
     
     $scope.checkUser = function(){
-        console.log($scope.U_ID);
-        $http.get('http://desktop-6upj287:7575/api/User/get/U_ID,U_Password/'+$scope.U_ID +","+$scope.U_Password)
-            .then( function (response) {    
-              $scope.mensaje = response.data;           
-        });
-    console.log(mensaje);
-    console.log('http://desktop-6upj287:7575/api/User/get/U_ID,U_Password/'+$scope.U_ID +","+$scope.U_Password);
- 
-
-       //     var div = document.createElement("div");
-            
-             
-            /*
-            var button = document.createElement("Button");
-            var textForButton = document.createTextNode("Release the alert");
-            button.appendChild(textForButton);
-            button.addEventListener("click", function(){
-                alert("Hi!");
-            });
-            
-        */
-       //    document.getElementById("loingAs").appendChild(div);
-       //    document.getElementById("loingAs").appendChild(button);
         
+       
+         $http.get('http://desktop-6upj287:7575/api/User/get/U_ID,U_Password/'+$scope.U_ID +","+$scope.U_Password)
+                .then(function (response) {
+                               mensaje=response.data[0];
+                               console.log(mensaje);
+                                 $scope.roles   = mensaje.q_role; 
+
+                                for(var x = 0; x < $scope.roles.length; x++){
+                                    $scope.roles[x] = JSON.parse($scope.roles[x]);
+                                }
+                                console.log(mensaje.q_id);
+                                console.log($scope.roles[0]);
+                                
+                                for(var x = 0; x < $scope.roles.length; x++){
+                                    $scope.roles[x] = JSON.parse($scope.roles[x]);
+                                    
+                                }
+             
+              
+                                if($scope.roles.length > 1){
+                                    
+                                    loginmodal.style.display = "block";
+                                }
+                                else if($scope.roles[0]==3){
+                                        console.log("Client");
+                                        localStorage.setItem("user", mensaje.q_id); 
+                                        window.location.assign("/pages/clientView.html")
+
+                                }
+                                else if($scope.roles[0]==1){
+                                       console.log("Admin"); 
+                                        localStorage.setItem("user", mensaje.q_id); 
+                                        window.location.assign("/pages/adminView.html")
+
+                                }          
+                                else if($scope.roles[0]==4) {
+                                        console.log("General");
+                                        localStorage.setItem("user", mensaje.q_id); 
+                                        window.location.assign("/pages/generalView.html")
+                                }  
+
+                                else if($scope.roles[0]==2) {
+                                    console.log("Engineer");
+                                      localStorage.setItem("user", mensaje.q_id); 
+                                    window.location.assign("/pages/employeeView.html")
+                                } 
+                                
+                 });
+        
+        
+        
+        
+        
+            
+                
     }
 
 
 
 
 
-    }]);
+}]);
+
+indexApp = angular.module('index')
+.controller('loginCtrl', ['$scope', '$http', function ($scope, $http) {
+    
+    
+    
+    
+    $scope.logClient = function(){
+
+                window.location.assign("/pages/clientView.html")        
+                 
+          
+    }
+    $scope.logEngineer = function(){
+
+                window.location.assign("/pages/employeeView.html")        
+                 
+          
+    }
+
+
+
+
+
+
+}]);
