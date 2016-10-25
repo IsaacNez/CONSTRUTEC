487ee513d34331a65493f96a1ff251b57f2beae9
IsaacNez <shak04995@gmail.com>
2016-10-25 01:16:29 -0600

    Se añade la última versión de la aplicación móvil.

--- CONSTRUTECMOVIL/app/src/main/java/construtec/construtec/AssignStage.java
+++ CONSTRUTECMOVIL/app/src/main/java/construtec/construtec/AssignStage.java
@@ -8,6 +8,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,6 +17,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -28,197 +30,232 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
 
 import cz.msebera.android.httpclient.Header;
+import cz.msebera.android.httpclient.entity.StringEntity;
 
 /**
  * Created by Isaac on 10/11/2016.
  */
 public class AssignStage extends Fragment {
-    View _assignStage;
-    TextView _projectsview;
-    List<Integer> _projectsID = new ArrayList<>();
-    List<String> _projectName = new ArrayList<>();
-    List<String> _projectPres = new ArrayList<>();
-    List<String> _stagesdata = new ArrayList<String>();
-    List<String> _stages = new ArrayList<>();
-    Integer _temp=0;
+    View _addstage;
 
-    Integer u_id = 0;
-    Integer u_code = 0;
-    String u_name = "";
-    String jsonArray = "";
+    List<String> _projectPres;
+    List<Integer> _projectsID;
+    List<String> _projectName;
+
+    Spinner _projectspinner;
+    Spinner _stagespinner;
+
+    ArrayAdapter<String> _projectadapter;
+    ArrayAdapter<String> _stageadapter;
+    private List<String> _stagesdata;
+
+    Button _add;
+
+    ListView _stagedetails;
+
+    EditText _stageSD;
+    EditText _stageDE;
+
+    Integer _position;
+    Integer _stagechoice;
+    private List<String> _stages;
+    private ArrayAdapter<String> _stagedetailadapter;
 
 
+    Integer _ucode=0;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        _assignStage = inflater.inflate(R.layout.assign_stage,container,false);
-        /*
-        Seteo de los datos al list view de los projectos.
-         */
-        try {
-            getprojects();
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        u_id = getArguments().getInt("u_id");
-        u_code = getArguments().getInt("u_code");
-        u_name = getArguments().getString("u_name");
-        jsonArray = getArguments().getString("jsonArray");
+        _addstage = inflater.inflate(R.layout.assign_stage,null);
+        _ucode = getArguments().getInt("u_code");
+        getprojects();
+        _projectspinner = (Spinner) _addstage.findViewById(R.id._projectspinner);
+        _stagespinner = (Spinner) _addstage.findViewById(R.id._stagespinner);
+        _add = (Button) _addstage.findViewById(R.id._addbutton);
+        _stagedetails = (ListView) _addstage.findViewById(R.id._stagedetails);
+        _stageDE = (EditText) _addstage.findViewById(R.id._stageDE);
+        _stageSD = (EditText) _addstage.findViewById(R.id._stageSD);
 
 
-        _projectsview = (TextView) _assignStage.findViewById(R.id.titleprojects);
-        _projectsview.setOnClickListener(new View.OnClickListener() {
+
+        _projectadapter = new ArrayAdapter<String>(_addstage.getContext(),
+                android.R.layout.simple_spinner_dropdown_item,_projectPres);
+        _projectspinner.setAdapter(_projectadapter);
+        _projectspinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void onClick(View v) {
-                final LayoutInflater _layout = LayoutInflater.from(_assignStage.getContext());
-                final View _showprojects = _layout.inflate(R.layout.chooseprojects, null);
-                final AlertDialog.Builder _windowpro = new AlertDialog.Builder(_assignStage.getContext());
-                _windowpro.setView(_showprojects);
-                ArrayAdapter<String> _listviewAdapter = new ArrayAdapter<String>(_showprojects.getContext(),
-                        android.R.layout.simple_list_item_1,_projectPres);
-                _windowpro.setCancelable(false)
-                        .setAdapter(_listviewAdapter, new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                getprojectdetails(_projectsID.get(which));
-                                start(_showprojects,which);
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                _position = position;
+                getStages(_projectsID.get(position));
+                _stageadapter = new ArrayAdapter<String>(_addstage.getContext(),
+                        android.R.layout.simple_spinner_dropdown_item, _stagesdata);
+                _stagespinner.setAdapter(_stageadapter);
+                getprojectdetails(_projectsID.get(_position));
 
 
-                            }
-                        });
-                AlertDialog _wa = _windowpro.create();
-                _windowpro.show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+        _stagespinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                _stagechoice = position;
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
 
             }
         });
 
+        _add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (_stageSD.getText().toString().matches("") |_stageDE.getText().toString().matches("")){
+                    Snackbar.make(_addstage,"You need to specify the End Date or the Start Date of your Stage",Snackbar.LENGTH_LONG).show();
+                }else {
+                    AsyncHttpClient httpClient = new AsyncHttpClient();
+                    String server = getString(R.string.url) + "projectxstage/post";
+                    try {
+                        JSONObject params = new JSONObject();
+                        params.put("p_id", _projectsID.get(_position));
+                        params.put("s_name", _stagesdata.get(_stagechoice));
+                        params.put("pxs_datestart", _stageSD.getText().toString());
+                        params.put("pxs_dateend", _stageDE.getText().toString());
+                        params.put("pxs_status", "Construyendo");
+                        StringEntity se = new StringEntity(params.toString());
+                        httpClient.post(_addstage.getContext(),server,se,"application/json",new JsonHttpResponseHandler(){
+                            @Override
+                            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                                Snackbar.make(_addstage,"You have successfully added a new Stage",Snackbar.LENGTH_LONG).show();
+                                getStages(_projectsID.get(_position));
+                                getprojectdetails(_projectsID.get(_position));
+                            }
+                        });
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    } catch (UnsupportedEncodingException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
 
-
-        return _assignStage;
+        return _addstage;
     }
-
-    private void start(View _showprojects, int which) {
-        final LayoutInflater _layoutstage = LayoutInflater.from(_showprojects.getContext());
-        final View _showprojectsstage = _layoutstage.inflate(R.layout.chooseprojects, null);
-        AlertDialog.Builder _windowprostages = new AlertDialog.Builder(_showprojects.getContext());
-        _windowprostages.setView(_showprojectsstage);
-        showstages(_windowprostages, _showprojectsstage, _projectsID.get(which));
-    }
-
-    public void getprojects() throws JSONException {
+    public void getprojects(){
         final AsyncHttpClient httpClient = new AsyncHttpClient();
-
-        String server = "http://isaac:7249/api/project/get/u_id/undefined";
+        _projectPres = new ArrayList<>();
+        _projectsID = new ArrayList<>();
+        _projectName = new ArrayList<>();
+        String server = getString(R.string.url)+"project/get/u_code/"+_ucode;
         httpClient.get(server, null, new JsonHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
                 try {
+
+
                     for (int i = 0; i < response.length(); i++) {
                         JSONObject _newJsonObject = response.getJSONObject(i);
                         _projectsID.add(_newJsonObject.getInt("p_id"));
                         _projectName.add(_newJsonObject.getString("p_name"));
                         _projectPres.add(_newJsonObject.getInt("p_id") + ": " + _newJsonObject.getString("p_name"));
                     }
+                    _projectadapter.notifyDataSetChanged();
+
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
+
                 httpClient.cancelAllRequests(true);
 
             }
         });
 
+
+
+
+
     }
 
-    public void getprojectdetails(int p_id){
+   public void getprojectdetails(int p_id){
         final AsyncHttpClient httpClient = new AsyncHttpClient();
         String server = "http://isaac:7249/api/projectdetails/get/p_id/"+p_id;
-        httpClient.get(server,null,new JsonHttpResponseHandler(){
-            @Override
-            public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
-                try{
-                    for(int i = 0;i<response.length();i++){
-                        JSONObject _newJSONObject = response.getJSONObject(i);
-                        JSONArray _newJSONArray = _newJSONObject.getJSONArray("stages");
-                        for(int j = 0; j<_newJSONArray.length();j++){
-
-                            JSONObject _stageJSONObject = _newJSONArray.getJSONObject(j);
-                            String action = "Name: " + _stageJSONObject.getString("s_name") + "\n"
-                                    + "Status: " + _stageJSONObject.getString("s_status") + "\n" +
-                                    "Start date: " + _stageJSONObject.getString("s_datestart") + "\n"
-                                    + "Start end: " + _stageJSONObject.getString("s_dateend")+"\n";
-                            if(!_stagesdata.contains(action))
-                                _stagesdata.add(action);
-
-                        }
-
-                    }
-                }catch (JSONException e){
-                    e.printStackTrace();
-                }
-
-            }
-        });
-
-
-    }
-
-    public void showstages(AlertDialog.Builder _windowpro, View _showprojects, Integer _integer){
-        ArrayAdapter<String> _listviewAdapter = new ArrayAdapter<String>(_showprojects.getContext(),
-                android.R.layout.simple_list_item_1,_stagesdata);
-        final Integer _project = _integer;
-        _windowpro.setCancelable(false)
-                .setAdapter(_listviewAdapter, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                    }
-                })
-                .setPositiveButton("Add", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.cancel();
-                        /**FragmentManager frg = getFragmentManager();
-                        frg.beginTransaction()
-                                .replace(R.layout.assign_stage,new addStage())
-                                .commit();*/
-                        Intent ca = new Intent(getActivity(),addStage.class);
-                        ca.putExtra("p_id",_project);
-                        ca.putExtra("u_id",u_id);
-                        ca.putExtra("u_code",u_code);
-                        ca.putExtra("u_name",u_name);
-                        ca.putExtra("jsonArray",jsonArray);
-                        startActivity(ca);
-                    }
-                });
-        AlertDialog _windowproco = _windowpro.create();
-        _windowpro.show();
-    }
-
-    public List<String> getStages(){
-        final AsyncHttpClient httpClient = new AsyncHttpClient();
-        String server = "http://isaac:7249/api/stage/get/p_id/undefined";
-        final List<String> _temp = new ArrayList<>();
-        httpClient.get(server,null, new JsonHttpResponseHandler(){
+       System.out.println("Project details: "+server);
+        httpClient.get(server, null, new JsonHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
                 try {
+                    System.out.println(response.toString());
 
+
+                        _stages = new ArrayList<>();
+                        _stagedetailadapter = new ArrayAdapter<String>(_addstage.getContext(), android.R.layout.simple_list_item_1,_stages);
+                        _stagedetails.setAdapter(_stagedetailadapter);
+                        for (int i = 0; i < response.length(); i++) {
+                            JSONObject _newJSONObject = response.getJSONObject(i);
+                            JSONArray _newJSONArray = _newJSONObject.getJSONArray("stages");
+                            if(_newJSONArray.length()>0) {
+                                for (int j = 0; j < _newJSONArray.length(); j++) {
+
+                                    JSONObject _stageJSONObject = _newJSONArray.getJSONObject(j);
+                                    String action = "Name: " + _stageJSONObject.getString("s_name") + "\n"
+                                            + "Status: " + _stageJSONObject.getString("s_status") + "\n" +
+                                            "Start date: " + _stageJSONObject.getString("s_datestart") + "\n"
+                                            + "Start end: " + _stageJSONObject.getString("s_dateend") + "\n";
+                                    if (!_stages.contains(action))
+                                        _stages.add(action);
+
+                                }
+                            }else{
+                                Snackbar.make(_addstage,"This project doesn't owns any stage",Snackbar.LENGTH_LONG).show();
+                            }
+
+
+                        }
+                        _stagedetailadapter.notifyDataSetChanged();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+
+
+    }
+
+    public List<String> getStages(Integer p_id){
+        final AsyncHttpClient httpClient = new AsyncHttpClient();
+        String server = "http://isaac:7249/api/stage/get/p_id/"+p_id;
+        final List<String> _temp = new ArrayList<>();
+        _stagesdata = new ArrayList<>();
+        httpClient.get(server, null, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
+                try {
+                    System.out.println(response.toString());
                     for (int i = 0; i < response.length(); i++) {
                         JSONObject _newJSON = response.getJSONObject(i);
-                        _temp.add(_newJSON.getString("s_name"));
+                        _stagesdata.add(_newJSON.getString("s_name"));
 
 
                     }
-                }catch (JSONException e){
+                    _stageadapter.notifyDataSetChanged();
+                } catch (JSONException e) {
                     e.printStackTrace();
                 }
             }
         });
-        _stages = _temp;
+
         return _temp;
     }
 }
